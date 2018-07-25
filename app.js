69b5ee7e95d12fbafa2494cad85d6772943e9862
DadidaOchieng <fred.ochieng@sinbad.co.ke>
2018-07-25 14:12:51 +0300

    Initial Project Set Up

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Create Express app
+const app = express();
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+// Make sure we are running node version 7.6+
+const [major, minor] = process.versions.node.split('.').map(parseFloat);
+if(major < 7 || (major === 7 && minor <= 5)) {
+  console.log("You are using an older version of node that does not support the latest greatest feature we are going to use");
+  process.exit();
+}
+
+//import environment varibales
+require('dotenv').config({ path: '.env'});
+
+//Connect to our Database
+mongoose.connect(process.env.DATABASE);
+mongoose.promise = global.Promise;
+mongoose.connection.on('error', err => {
+  console.error(err.message);
+});
+
+// Import models
+// require('./models');
+
+//Start App
+const app = require('./app');
+app.set('port', process.env.PORT || 9090);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Server running on PORT ${server.address().port}`);
+});
